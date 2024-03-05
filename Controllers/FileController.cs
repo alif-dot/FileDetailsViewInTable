5573f0c51ac56a19600b963b10e1fad54378bb72
alif-dot <alif17648@outlook.com>
2024-03-05 15:51:39 +0600

    Code Optimize

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -7,38 +7,16 @@ namespace FileDetailsViewTable.Controllers
 {
     public class FileController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    string folderPath = @"E:\PC's Software\Tools";
-
-        //    List<FileTable> files = GetZipFiles(folderPath);
-
-        //    // Sort the files list in ascending order based on DateModified
-        //    files = files.OrderBy(f => f.DateModified).ToList();
-        //    //files = files.OrderByDescending(f => f.DateModified).ToList();
-        //    return View(files);
-        //}
-
-        private const int PageSize = 10; // Set the number of items per page
+        private const int PageSize = 10;
 
         public IActionResult Index(int? page)
         {
             string folderPath = @"E:\PC's Software\Tools";
-
             List<FileTable> allFiles = GetZipFiles(folderPath);
-
-            // Sort the files list in ascending order based on DateModified
             allFiles = allFiles.OrderBy(f => f.DateModified).ToList();
-
-            // Paginate the files
             var pagedList = allFiles.ToPagedList(page ?? 1, PageSize);
 
             return View(pagedList);
-        }
-
-        private List<FileTable> PaginateFiles(List<FileTable> allFiles, int page)
-        {
-            return allFiles.Skip((page - 1) * PageSize).Take(PageSize).ToList();
         }
 
         private List<FileTable> GetZipFiles(string folderPath)
